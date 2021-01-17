4fc82e6ef430ce6040e3cb561eeffa72aacef792
Ismail Simsek <ismail.simsek@ppro.com>
2021-01-17 22:09:48 +0100

    more changes

--- debezium-server-batch/src/main/java/io/debezium/server/batch/BatchUtil.java
+++ debezium-server-batch/src/main/java/io/debezium/server/batch/BatchUtil.java
@@ -8,18 +8,9 @@
 
 package io.debezium.server.batch;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.iceberg.Schema;
-import org.apache.iceberg.data.GenericRecord;
-import org.apache.iceberg.types.Types;
 import org.apache.spark.sql.types.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -93,71 +84,6 @@ public class BatchUtil {
 
   }
 
-  public static Schema getIcebergSchema(JsonNode eventSchema) {
-    LOGGER.debug(eventSchema.toString());
-    return getIcebergSchema(eventSchema, "", -1);
-  }
-
-  public static Schema getIcebergSchema(JsonNode eventSchema, String schemaName, int columnId) {
-    List<Types.NestedField> schemaColumns = new ArrayList<>();
-    String schemaType = eventSchema.get("type").textValue();
-    LOGGER.debug("Converting Schema of: {}::{}", schemaName, schemaType);
-    for (JsonNode jsonSchemaFieldNode : eventSchema.get("fields")) {
-      columnId++;
-      String fieldName = jsonSchemaFieldNode.get("field").textValue();
-      String fieldType = jsonSchemaFieldNode.get("type").textValue();
-      LOGGER.debug("Processing Field: [{}] {}.{}::{}", columnId, schemaName, fieldName, fieldType);
-      switch (fieldType) {
-        case "int8":
-        case "int16":
-        case "int32": // int 4 bytes
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.IntegerType.get()));
-          break;
-        case "int64": // long 8 bytes
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.LongType.get()));
-          break;
-        case "float8":
-        case "float16":
-        case "float32": // float is represented in 32 bits,
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.FloatType.get()));
-          break;
-        case "float64": // double is represented in 64 bits
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.DoubleType.get()));
-          break;
-        case "boolean":
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.BooleanType.get()));
-          break;
-        case "string":
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.StringType.get()));
-          break;
-        case "bytes":
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.BinaryType.get()));
-          break;
-        case "array":
-          throw new RuntimeException("'" + fieldName + "' has Array type, Array type not supported!");
-          //schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.ListType.ofOptional()));
-          //break;
-        case "map":
-          throw new RuntimeException("'" + fieldName + "' has Map type, Map type not supported!");
-          //schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.StringType.get()));
-          //break;
-        case "struct":
-          throw new RuntimeException("Event schema containing nested data '" + fieldName + "' cannot process nested" +
-              " data!");
-//          //recursive call
-//          Schema subSchema = SchemaUtil.getIcebergSchema(jsonSchemaFieldNode, fieldName, ++columnId);
-//          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.StructType.of(subSchema.columns())));
-//          columnId += subSchema.columns().size();
-//          break;
-        default:
-          // default to String type
-          schemaColumns.add(Types.NestedField.optional(columnId, fieldName, Types.StringType.get()));
-          break;
-      }
-    }
-    return new Schema(schemaColumns);
-  }
-
   // @TODO replace!
   public static StructType getEventSparkDfSchema(String event) throws JsonProcessingException {
     JsonNode jsonNode = new ObjectMapper().readTree(event);
@@ -175,75 +101,4 @@ public class BatchUtil {
         && jsonNode.get("schema").get("fields").isArray();
   }
 
-  public static GenericRecord getIcebergRecord(Schema schema, JsonNode data) throws InterruptedException {
-    return BatchUtil.getIcebergRecord(schema.asStruct(), data);
-  }
-
-  public static GenericRecord getIcebergRecord(Types.StructType nestedField, JsonNode data) throws InterruptedException {
-    Map<String, Object> mappedResult = new HashMap<>();
-    LOGGER.debug("Processing nested field : " + nestedField);
-
-    for (Types.NestedField field : nestedField.fields()) {
-      if (data == null || !data.has(field.name()) || data.get(field.name()) == null) {
-        mappedResult.put(field.name(), null);
-        continue;
-      }
-      jsonToGenericRecord(mappedResult, field, data.get(field.name()));
-    }
-    return GenericRecord.create(nestedField).copy(mappedResult);
-  }
-
-  private static void jsonToGenericRecord(Map<String, Object> mappedResult, Types.NestedField field,
-                                          JsonNode node) throws InterruptedException {
-    LOGGER.debug("Processing Field:" + field.name() + " Type:" + field.type());
-
-    switch (field.type().typeId()) {
-      case INTEGER: // int 4 bytes
-        mappedResult.put(field.name(), node.asInt());
-        break;
-      case LONG: // long 8 bytes
-        mappedResult.put(field.name(), node.asLong());
-        break;
-      case FLOAT: // float is represented in 32 bits,
-        mappedResult.put(field.name(), node.floatValue());
-        break;
-      case DOUBLE: // double is represented in 64 bits
-        mappedResult.put(field.name(), node.asDouble());
-        break;
-      case BOOLEAN:
-        mappedResult.put(field.name(), node.asBoolean());
-        break;
-      case STRING:
-        mappedResult.put(field.name(), node.asText());
-        break;
-      case BINARY:
-        try {
-          mappedResult.put(field.name(), node.binaryValue());
-        } catch (IOException e) {
-          LOGGER.error("Failed converting '" + field.name() + "' binary value to iceberg record", e);
-          throw new InterruptedException("Failed Processing Event!" + e.getMessage());
-        }
-        break;
-      case LIST:
-        mappedResult.put(field.name(), jsonObjectMapper.convertValue(node, List.class));
-        break;
-      case MAP:
-        mappedResult.put(field.name(), jsonObjectMapper.convertValue(node, Map.class));
-        break;
-      case STRUCT:
-        throw new RuntimeException("Cannot process recursive records!");
-        // Disabled because cannot recursively process StructType/NestedField
-//        // recursive call to get nested data/record
-//        Types.StructType nestedField = Types.StructType.of(field);
-//        GenericRecord r = getIcebergRecord(schema, nestedField, node.get(field.name()));
-//        mappedResult.put(field.name(), r);
-//        // throw new RuntimeException("Cannot process recursive record!");
-//        break;
-      default:
-        // default to String type
-        mappedResult.put(field.name(), node.asText());
-        break;
-    }
-
-  }
 }
--- debezium-server-batch/src/test/java/io/debezium/server/batch/TestBatchUtil.java
+++ debezium-server-batch/src/test/java/io/debezium/server/batch/TestBatchUtil.java
@@ -11,15 +11,11 @@ package io.debezium.server.batch;
 import io.debezium.serde.DebeziumSerdes;
 import io.debezium.util.Testing;
 
-import java.io.IOException;
 import java.util.Collections;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.iceberg.Schema;
-import org.apache.iceberg.data.GenericRecord;
-import org.apache.iceberg.types.Types;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.spark.sql.types.StructType;
 import org.junit.jupiter.api.Test;
@@ -57,63 +53,6 @@ class TestBatchUtil {
     assertNotNull(s);
     assertTrue(s.catalogString().contains("before:struct<id"));
     assertTrue(s.catalogString().contains("after:struct<id"));
-  }
-
-  @Test
-  public void testNestedIcebergSchema() throws JsonProcessingException {
-    Schema s = BatchUtil.getIcebergSchema(new ObjectMapper().readTree(serdeWithSchema).get("schema"));
-    // StructType ss = ConsumerUtil.getEventSparkDfSchema(serdeWithSchema);
-    assertNotNull(s);
-    assertEquals(s.findField("ts_ms").fieldId(), 29);
-    assertEquals(s.findField(7).name(), "after");
-    assertTrue(s.asStruct().toString().contains("source: optional struct<"));
-    assertTrue(s.asStruct().toString().contains("after: optional struct<"));
-  }
-
-  @Test
-  public void testNestedIcebergSchema2() throws JsonProcessingException {
-    Schema s = BatchUtil.getIcebergSchema(new ObjectMapper().readTree(serdeWithSchema2));
-    // assertEquals(s.asStruct().toString(), "xx");
-    assertTrue(s.asStruct().toString().contains("source: optional struct<"));
-    assertTrue(s.asStruct().toString().contains("after: optional struct<"));
-  }
-
-  @Test
-  public void testUnwrapJsonRecord() throws IOException, InterruptedException {
-    JsonNode event = new ObjectMapper().readTree(unwrapWithSchema).get("payload");
-    Schema schema = BatchUtil.getIcebergSchema(new ObjectMapper().readTree(unwrapWithSchema).get("schema"));
-    GenericRecord record = BatchUtil.getIcebergRecord(schema.asStruct(), event);
-    assertEquals("orders", record.getField("__table").toString());
-    assertEquals(16850, record.getField("order_date"));
-  }
-
-  @Test
-  public void testNestedJsonRecord() throws IOException, InterruptedException {
-    JsonNode event = new ObjectMapper().readTree(serdeWithSchema).get("payload");
-    Schema schema = BatchUtil.getIcebergSchema(new ObjectMapper().readTree(serdeWithSchema).get("schema"));
-
-    System.out.println(schema.asStruct().field("before"));
-    System.out.println(schema.asStruct().fields());
-    System.out.println(schema.asStruct().field("id"));
-    System.out.println(
-        Types.StructType.of(schema.findField("before")).field("before")
-    );
-    fail();
-
-    assert schema != null;
-    GenericRecord record = BatchUtil.getIcebergRecord(schema.asStruct(), event);
-    System.out.println(record);
-    System.out.println(record.getField("after").toString());
-    System.out.println(record.getField("after").getClass().getSimpleName());
-    System.out.println(record.getClass().getSimpleName());
-
-    // unwrapped
-    JsonNode eventUw = new ObjectMapper().readTree(unwrapWithSchema).get("payload");
-    Schema schemaUw = BatchUtil.getIcebergSchema(new ObjectMapper().readTree(unwrapWithSchema).get("schema"));
-
-    GenericRecord recordUw = BatchUtil.getIcebergRecord(schemaUw.asStruct(), eventUw);
-
-    fail();
   }
 
   @Test
