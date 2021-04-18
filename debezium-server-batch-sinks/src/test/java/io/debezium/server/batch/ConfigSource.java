b4424754b0cae3c2a8a47bcfdd6f87e33be9ab0e
Ismail Simsek <ismail.simsek@ppro.com>
2021-04-18 21:37:35 +0200

    hudi consumer

--- debezium-server-batch-sinks/src/test/java/io/debezium/server/batch/ConfigSource.java
+++ debezium-server-batch-sinks/src/test/java/io/debezium/server/batch/ConfigSource.java
@@ -33,7 +33,7 @@ public class ConfigSource extends TestConfigSource {
     s3Test.put("debezium.sink.batch.row-limit", "2");
     s3Test.put("debezium.sink.batch.time-limit", "10"); // second
 
-    s3Test.put("debezium.source.max.batch.size", "1");
+    s3Test.put("debezium.source.max.batch.size", "100");
     s3Test.put("debezium.source.poll.interval.ms", "5000");
 
     s3Test.put("debezium.source.database.history.kafka.bootstrap.servers", "kafka:9092");
