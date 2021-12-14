84050b562307c3b58abbfdbe95d77987b3614421
Ismail Simsek <gel.yine.gel@hotmail.com>
2021-12-14 11:03:57 +0100

    Minor improvements

--- debezium-server-batch-sinks/src/main/java/io/debezium/server/batch/bigquery/BatchBigqueryChangeConsumer.java
+++ debezium-server-batch-sinks/src/main/java/io/debezium/server/batch/bigquery/BatchBigqueryChangeConsumer.java
@@ -9,9 +9,11 @@
 package io.debezium.server.batch.bigquery;
 
 import io.debezium.DebeziumException;
+import io.debezium.server.batch.AbstractChangeConsumer;
 import io.debezium.server.batch.BatchEvent;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.channels.Channels;
@@ -19,12 +21,19 @@ import java.nio.file.Files;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.*;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  * Implementation of the consumer that delivers the messages into Amazon S3 destination.
@@ -33,11 +42,84 @@ import com.google.cloud.bigquery.*;
  */
 @Named("bigquerybatch")
 @Dependent
-public class BatchBigqueryChangeConsumer extends AbstractBigqueryChangeConsumer {
+public class BatchBigqueryChangeConsumer extends AbstractChangeConsumer {
+
+  @ConfigProperty(name = "debezium.sink.batch.destination-regexp", defaultValue = "")
+  protected Optional<String> destinationRegexp;
+  @ConfigProperty(name = "debezium.sink.batch.destination-regexp-replace", defaultValue = "")
+  protected Optional<String> destinationRegexpReplace;
+  @Inject
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.dataset", defaultValue = "")
+  Optional<String> bqDataset;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.location", defaultValue = "US")
+  String bqLocation;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.project", defaultValue = "")
+  Optional<String> gcpProject;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.createDisposition", defaultValue = "CREATE_IF_NEEDED")
+  String createDisposition;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.partitionField", defaultValue = "__source_ts")
+  String partitionField;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.partitionType", defaultValue = "MONTH")
+  String partitionType;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.allowFieldAddition", defaultValue = "true")
+  Boolean allowFieldAddition;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.allowFieldRelaxation", defaultValue = "true")
+  Boolean allowFieldRelaxation;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.credentialsFile", defaultValue = "")
+  Optional<String> credentialsFile;
+  @ConfigProperty(name = "debezium.sink.bigquerybatch.cast-deleted-field", defaultValue = "false")
+  Boolean castDeletedField;
+
+  BigQuery bqClient;
+  TimePartitioning timePartitioning;
+  List<JobInfo.SchemaUpdateOption> schemaUpdateOptions = new ArrayList<>();
+  
   @PostConstruct
   void connect() throws InterruptedException {
     this.initizalize();
   }
+
+  public void initizalize() throws InterruptedException {
+
+    if (gcpProject.isEmpty()) {
+      throw new InterruptedException("Please provide a value for `debezium.sink.bigquerybatch.project`");
+    }
+
+    if (bqDataset.isEmpty()) {
+      throw new InterruptedException("Please provide a value for `debezium.sink.bigquerybatch.dataset`");
+    }
+
+    if (credentialsFile.isEmpty()) {
+      throw new InterruptedException("Please provide a value for `debezium.sink.bigquerybatch.credentialsFile`");
+    }
+
+    GoogleCredentials credentials;
+    try (FileInputStream serviceAccountStream = new FileInputStream(credentialsFile.get())) {
+      credentials = GoogleCredentials.fromStream(serviceAccountStream);
+    } catch (IOException e) {
+      throw new DebeziumException(e);
+    }
+
+    bqClient = BigQueryOptions.newBuilder()
+        .setCredentials(credentials)
+        .setProjectId(gcpProject.get())
+        .setLocation(bqLocation)
+        .build()
+        .getService();
+
+    timePartitioning =
+        TimePartitioning.newBuilder(TimePartitioning.Type.valueOf(partitionType)).setField(partitionField).build();
+
+    if (allowFieldAddition) {
+      schemaUpdateOptions.add(JobInfo.SchemaUpdateOption.ALLOW_FIELD_ADDITION);
+    }
+    if (allowFieldRelaxation) {
+      schemaUpdateOptions.add(JobInfo.SchemaUpdateOption.ALLOW_FIELD_RELAXATION);
+    }
+
+    super.initizalize();
+  }
+
 
   @Override
   public long uploadDestination(String destination, List<BatchEvent> data) throws InterruptedException {
@@ -101,5 +183,22 @@ public class BatchBigqueryChangeConsumer extends AbstractBigqueryChangeConsumer 
       jsonlines.delete();
     }
   }
+
+
+  private TableId getTableId(String destination) {
+    final String tableName = destination
+        .replaceAll(destinationRegexp.orElse(""), destinationRegexpReplace.orElse(""))
+        .replace(".", "_");
+    return TableId.of(gcpProject.get(), bqDataset.get(), tableName);
+  }
+
+  @Override
+  public JsonNode getPayload(String destination, Object val) {
+    JsonNode pl = valDeserializer.deserialize(destination, getBytes(val));
+    // used to partition tables __source_ts
+    ((ObjectNode) pl).put("__source_ts", pl.get("__source_ts_ms").longValue() / 1000);
+    return pl;
+  }
+
 
 }
