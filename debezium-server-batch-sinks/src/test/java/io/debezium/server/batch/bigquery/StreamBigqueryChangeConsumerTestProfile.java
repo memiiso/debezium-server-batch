96e2711476289ee493a8988ae1524ec9c2ec1720
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-02-25 18:46:19 +0100

    Add bigquery stream consumer

--- debezium-server-batch-sinks/src/test/java/io/debezium/server/batch/bigquery/StreamBigqueryChangeConsumerTestProfile.java
+++ debezium-server-batch-sinks/src/test/java/io/debezium/server/batch/bigquery/StreamBigqueryChangeConsumerTestProfile.java
@@ -13,23 +13,23 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BatchBigqueryChangeConsumerTestProfile implements QuarkusTestProfile {
+public class StreamBigqueryChangeConsumerTestProfile implements QuarkusTestProfile {
 
   //This method allows us to override configuration properties.
   @Override
   public Map<String, String> getConfigOverrides() {
     Map<String, String> config = new HashMap<>();
 
-    config.put("debezium.sink.type", "bigquerybatch");
+    config.put("debezium.sink.type", "bigquerystream");
     config.put("debezium.source.table.include.list", "inventory.test_date_table,inventory.customers,inventory.geom");
     //
-    config.put("debezium.sink.bigquerybatch.project", "test");
-    config.put("debezium.sink.bigquerybatch.dataset", "test");
-    config.put("debezium.sink.bigquerybatch.location", "EU");
-    //config.put("debezium.sink.bigquerybatch.credentialsFile", "/path/to/application_credentials.json");
+    config.put("debezium.sink.bigquerystream.project", "test");
+    config.put("debezium.sink.bigquerystream.dataset", "test");
+    config.put("debezium.sink.bigquerystream.location", "EU");
     // logging
     config.put("quarkus.log.category.\"io.debezium.server.batch\".level", "INFO");
-    config.put("quarkus.log.category.\"io.debezium.server.batch.bigquery.BatchBigqueryChangeConsumer\".level", "DEBUG");
+    config.put("quarkus.log.category.\"io.debezium.server.batch.bigquery.StreamBigqueryChangeConsumer\".level",
+        "DEBUG");
     config.put("quarkus.log.category.\"com.google.cloud.bigquery\".level", "INFO");
     return config;
   }
