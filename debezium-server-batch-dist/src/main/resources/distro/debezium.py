ae6c37988af437a4db21c4d22d293489c621c905
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-05-28 10:37:20 +0200

    Add python runner

--- debezium-server-batch-dist/src/main/resources/distro/debezium.py
+++ debezium-server-batch-dist/src/main/resources/distro/debezium.py
@@ -84,8 +84,10 @@ def main():
                         help='Directory of debezium server application')
     parser.add_argument('--conf_dir', type=str, default=None,
                         help='Directory of application.properties')
+    parser.add_argument('--java_home', type=str, default=None,
+                        help='JAVA_HOME directory')
     _args, args = parser.parse_known_args()
-    ds = Debezium(debezium_dir=_args.debezium_dir, conf_dir=_args.conf_dir)
+    ds = Debezium(debezium_dir=_args.debezium_dir, conf_dir=_args.conf_dir, java_home=_args.java_home)
     ds.run(*args)
 
 
